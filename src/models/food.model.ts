a523d8e827d20656ee4280220962fa72b628bdcf
ahmadibni <ahmadibni007@gmail.com>
2025-06-04 14:27:16 +0700

    add timestamps to food model

--- src/models/food.model.ts
+++ src/models/food.model.ts
@@ -11,32 +11,37 @@ interface Food {
 
 const schema = mongoose.Schema;
 
-const foodSchema = new schema<Food>({
-  name: {
-    type: String,
-    required: true,
+const foodSchema = new schema<Food>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  isAvailable: {
-    type: Boolean,
-    default: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Food = mongoose.model<Food>("Food", foodSchema);
 

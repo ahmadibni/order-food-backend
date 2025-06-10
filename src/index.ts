7796de4c99cc8daa656ad3f6bc54b1042305d91f
ahmadibni <ahmadibni007@gmail.com>
2025-06-10 20:39:26 +0700

    add image and cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { PORT } from "./config/env";
+import cors from "cors";
 import connect from "./config/database";
 import bodyParser from "body-parser";
 import router from "./routes/api";
@@ -8,6 +9,7 @@ const app = express();
 
 connect();
 
+app.use(cors());
 app.use(express.static("files"));
 app.use(bodyParser.json());
 app.use("/api", router);
